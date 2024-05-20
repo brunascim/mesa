2af0add75bc46d31781a26923b568477d175cde1
brunascim <127804993+brunascim@users.noreply.github.com>
2024-05-20 09:49:39 -0300

    aula 1- 20-05

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <view>
+      <h1>Projeto TCC</h1>
+    </view>
+  );
+}
