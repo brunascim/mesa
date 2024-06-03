dc879e2d88f9a6eb887793bcd5b7c66db311feb4
brunascim <127804993+brunascim@users.noreply.github.com>
2024-06-03 09:51:01 -0300

    aula1 -03-06

---
+++ src/app/temp/page.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import Link from "next/link";
+
+export default function Telas() {
+  return (
+    <div>
+      <h1>Telas</h1>
+    </div>
+  );
+}
