1956a0230736ab4136fe2c20e42f9395e4bbe076
brunascim <127804993+brunascim@users.noreply.github.com>
2024-06-03 11:38:15 -0300

    aula 2 03-06

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,12 @@
-import Image from "next/image";
+
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       <h1>Projeto TCC</h1>
+      <Link  href={"/compra"}>Compra</Link>
     </div>
   );
 }
--- src/app/temp/page.jsx
+++ src/app/temp/page.jsx
@@ -1,11 +1,19 @@
-import Image from "next/image";
+
 import styles from "./page.module.css";
 import Link from "next/link";
 
-export default function Telas() {
+export default function Home() {
   return (
-    <div>
-      <h1>Telas</h1>
+    <div className="container" >
+      <h1>Links</h1>
+      <Link  href={"/"}><p className={styles.linkTemp}>Home</p></Link>
+      <Link href={"/compra"}><p className={styles.linkTemp}>Compra</p></Link>
+      <Link href={"/duvidas"}><p className={styles.linkTemp}>Duvidas</p></Link>
+      <Link href={"/homelogin"}><p className={styles.linkTemp}>Home Login</p></Link>
+      <Link href={"/compra"}><p className={styles.linkTemp}>Compra</p></Link>
+      <Link href={"/usuarios/cadusuario"}><p className={styles.linkTemp}>Cadastro de usuario</p></Link>
+      <Link href={"/usuarios/login"}><p className={styles.linkTemp}>Login</p></Link>
+
     </div>
   );
 }
